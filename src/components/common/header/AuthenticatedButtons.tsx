7855c86e5fe6a40c0bea2426232a5ecd112caebc
bschaaf1017 <bschaaf1017@gmail.com>
2021-09-13 16:22:28 -0400

    added style and another element to AuthenticatedButtons

--- src/components/common/header/AuthenticatedButtons.tsx
+++ src/components/common/header/AuthenticatedButtons.tsx
@@ -1,12 +1,22 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
 import SignOut from '../../../auth/SignOut'
 
 const AuthenticatedButtons = (): React.ReactElement => {
+
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
+      <div>Open Workspace</div>
       <SignOut />
     </div>
   )
 }
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+  }
+}))
 
 export default AuthenticatedButtons
